1329fffab46daa3dba751a33b47e3f2988b48ea9
Vasiliy <shumchenko.94@mail.ru>
2022-10-14 19:58:38 +0300

    Natural numbers rekursia

---
+++ Task_64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+
+    Console.Write("Введите число N: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    NaturalNumbers(number);
+
+    void NaturalNumbers(int number)
+    {
+        if (number == 0) return;
+        Console.Write(" " + number);
+        NaturalNumbers(number - 1);
+        
+    }
+    Console.WriteLine();
