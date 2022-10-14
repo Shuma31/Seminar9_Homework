002e4a050157d04db799d579d2238b9a1ba6f4e1
Vasiliy <shumchenko.94@mail.ru>
2022-10-14 22:36:51 +0300

    Summa naturals numbers from M to N

---
+++ Task_66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите число M: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+int temp = numberOne;
+if (numberOne > numberTwo)
+{
+    numberOne = numberTwo;
+    numberTwo = temp;
+}
+SummaNumbers(numberOne, numberTwo, temp = 0);
+
+void SummaNumbers(int numberOne, int numberTwo, int sum)
+{
+    if (numberOne > numberTwo) 
+    {
+        Console.WriteLine("Сумма чисел от M до N = " + sum);
+        return;
+    }
+    sum = sum + (numberOne++);
+    SummaNumbers(numberOne, numberTwo, sum);
+}
